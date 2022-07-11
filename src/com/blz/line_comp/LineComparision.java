ffe7b1b0d35756b3ebf999f75dbfb2135df5116a
akashsathe063 <akashsathe063@gmail.com>
2022-07-11 13:00:39 +0530

    welcome to line comparision computation

---
+++ src/com/blz/line_comp/LineComparision.java
@@ -0,0 +1,7 @@
+package com.blz.line_comp;
+
+public class LineComparision {
+    public static void main(String[] args) {
+        System.out.println("welcome to line comparision computation");
+    }
+}
